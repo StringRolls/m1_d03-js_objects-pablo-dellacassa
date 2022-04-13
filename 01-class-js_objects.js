f87f2891cd5bcf934e0ad9c981d7824f83652817
Pablo Dellacassa <pdellacassa@gmail.com>
2022-04-13 15:18:59 +0200

    Update class and exercises

--- 01-class-js_objects.js
+++ 01-class-js_objects.js
@@ -1,74 +1,139 @@
 // Class code examples
 
+/* 
+
+An object is relationship, a collection, a record, which has properties 
+
+Object express relationships, array express order
+
+Objects protect with curly brackets variables with lables and values
+
+*/
+
 const person1 = {
-    firstName : "Chee Kean",
-    weight : 231,
-    height : 1.7
-}
+  // person 1 is an instantiation of an object
+  firstName: "Chee Kean",
+  weight: 231,
+  height: 1.7,
+};
 
 const person2 = {
-    firstName : "Marco Santo",
-    weight : 231,
-    height: 1.7
-}
+  firstName: "Marco Santo",
+  weight: 231,
+  height: 1.7,
+};
 
-let firstName3 = "Carlos Max"
-let weight3=  231
-let height3 = 1.7
+const person3 = {
+  firstName: "Carlitro",
+  weight: 231,
+  height: 1.7,
+};
 
-
-let firstName4 = "Caroluna"
-let weight4=  231
-let height4 = 1.7
+const person4 = {
+  firstName: "Caroluna",
+  weight: 231,
+  height: 1.7,
+  address: {
+    street: "Carrer de Pamplona",
+    number: "96",
+    planta: "PB",
+    piso: "1º",
+  },
+};
 
 // Accessing a property with dot notation
 console.log(
-    person1.firstName,
-    person1.weight
-)
+  "Info about Caroluna",
+  person4.firstName,
+  person4.weight,
+  person4.address.piso,
+  "\n"
+);
 
-// Accessing a property with bracket notation (indirect acces, or dynamic)
-console.log(person1["weight"])
+// Accessing a property with bracket notation or arrays (indirect access, or dynamic)
 
+// A generic obj could be access with dot notation too (static, accept values)
 
-const personKey = ["firstName", "weight", "height" ]
+console.log(person1["weight"]);
 
-for(key of personKey){
-    console.log(`person1 ${key}: `, person1[key])
+const personKey = ["firstName", "weight", "height"];
+const personAddressKey = ["street", "number", "planta", "piso"];
+
+console.log("_____________________");
+for (const key of personKey) {
+  console.log(`person4 ${key}: `, person4[key]);
 }
 
-person1.nationality = "Italian"
+for (const key of personAddressKey) {
+  console.log(`person4 ${key}: `, person4.address[key]);
+}
+console.log("_____________________");
+console.log("All the objects properties: ", Object.keys(person1));
+console.log("_____________________");
 
-console.log(person1.favFood)
+const allTheKeys = Object.keys(person4);
+console.log("The keys array: ", allTheKeys);
 
-person1["favFood"] = "pizza"
+for (const oneKey of Object.keys(person1)) {
+  console.log(`A key from person1 ${oneKey}: `, person1[oneKey]);
+}
+console.log("_____________________");
 
-const newKey = `favourite book`
-person1[newKey] = "guide to galaxy"
+person1.nationality = "Italian";
 
-console.log("person1: ", person1)
+console.log(person1.favFood);
 
-for (key in person1) console.log(person1[key])
+person1["favFood"] = "pizza";
+
+const newKey = `favourite book`;
+person1[newKey] = "guide to galaxy";
+
+console.log("person1: ", person1);
+
+for (key in person1) console.log(person1[key]);
 
 // Object.keys(person1)
-for (key of Object.keys(person1)) console.log(person1[key])
 
-console.log("accessing a number", person1[1087643])
+/* 
+The Object.keys() method returns an array of a given 
+object's own enumerable property names, iterated in the 
+same order that a normal loop would. 
+*/
 
-console.log("reading index from keys", Object.keys(person1)[1])
-console.log("object lenght", person1.length)
+for (key of Object.keys(person1)) console.log(person1[key]);
+
+console.log("accessing a number", person1[1087643]);
+
+console.log("reading index from keys", Object.keys(person1)[1]);
+
+// Objects don't have lenght but the dot notation will look for the label "lenght"
+console.log("object lenght", person1.length); // wrong way because person is an object no an array
+
+// To get the number of properties you will use the Object.keys().length trick
+console.log("Number of the object properties", Object.keys(person1).length);
+
+/*
+The Object.values() method returns an array of a given
+object's own enumerable property values, in the same
+order as that provided by a for...in loop.
+(The only difference is that a for...in loop enumerates
+properties in the prototype chain as well.)
+*/
+
+for (const oneValue in person4){
+    console.log("This is one value from the object", person4[oneValue])
+}
+
+
 
 const tween1 = {
-    name: "Carlos",
-    age: 24
-}
+  name: "Carlos",
+  age: 24,
+};
 
 const tween2 = {
-    name: "Carlos",
-    age: 24
-}
+  name: "Carlos",
+  age: 24,
+};
 
-console.log(
-    tween1 === tween2,
-    "Carlos" === "Carlos"
-    )
+console.log(tween1 === tween2, "Carlos" === "Carlos");
